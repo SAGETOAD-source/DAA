7fe5538ace72bbda4f27da4f73c23d00543017d3
SAGETOAD-source <b2hkrishna@gmail.com>
2026-01-10 01:10:21 +0530

    binarysearch available

--- DAA/bubble_sort.c
+++ DAA/bubble_sort.c
@@ -9,15 +9,13 @@ void swap(int *x, int *y)
 
 void bubble_sort(int arr[], int n)
 {
-    for (int i = 0; i < n - 1; i++) // outer loop
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < n - i - 1; j++) // inner loop
+        for (int j = 0; j < n - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(&arr[j], &arr[j + 1]);
-
-                // Print array after every swap
                 printf("Step: ");
                 for (int k = 0; k < n; k++)
                     printf("%d ", arr[k]);
